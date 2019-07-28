e3e395e401daca084730a69d1202967158ed9034
Jillian Anderson Slate <jillian.anderson17@gmail.com>
2019-07-28 20:50:55 +0200

    add nav bar, begin styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Calendar from './components/Calendar'
 import List from './components/List'
+import TopNav from './components/TopNav';
 import './App.css';
 
 const App = () => {
   return (
     <div className="app-container">
+      <TopNav />
       <List />
       <Calendar />
     </div>
---
+++ src/components/TopNav.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  top: 0;
+  background: #212121;
+  color: #ccc;
+`
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  margin: 0;
+  padding: 0;
+`
+
+const ListItem = styled.li`
+  height: 100%;
+  flex: 1;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  transition: all 0.3s ease-in;
+  cursor: pointer;
+`
+
+const TopNav = props => {
+
+  return (
+    <Nav>
+      <List>
+        <ListItem>Inspo</ListItem>
+        <ListItem>Moods</ListItem>
+        <input placeholder="Search" />
+      </List>
+    </Nav>
+  )
+}
+
+export default TopNav;
